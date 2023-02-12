405a3730f318abdd7a3ad82028b9504b3dbcfcb0
Dennis Liu <liusx880630@gmail.com>
2023-02-13 00:29:33 +0800

    Create files for MPU6500

--- Core/Inc/devices/mpu6500.h
+++ Core/Inc/devices/mpu6500.h
@@ -2,12 +2,11 @@
  * mpu6500.h
  *
  *  Created on: Feb 12, 2023
- *      Author: liusx
+ *      Author: Dennis
  */
 
 #ifndef INC_DEVICES_MPU6500_H_
 #define INC_DEVICES_MPU6500_H_
-
 
 
 #endif /* INC_DEVICES_MPU6500_H_ */
--- Core/Src/devices/mpu6500.c
+++ Core/Src/devices/mpu6500.c
@@ -2,7 +2,7 @@
  * mpu6500.c
  *
  *  Created on: Feb 12, 2023
- *      Author: liusx
+ *      Author: Dennis
  */
 
 
