a8743579281c0c886a3d8e6165acee0d94e525d5
Dennis Liu <liusx880630@gmail.com>
2023-03-23 03:30:35 +0800

    fix: 修正 415fc85-1
    說明:
    忘記將新檔案加入

---
+++ Core/Inc/lra/lra_spi_devices.h
@@ -0,0 +1,22 @@
+/*
+ * lra_spi_devices.h
+ *
+ *  Created on: Mar 22, 2023
+ *      Author: Dennis Liu
+ */
+
+#ifndef INC_LRA_SPI_DEVICES_H_
+#define INC_LRA_SPI_DEVICES_H_
+
+/* includes */
+
+#include "lra/lra_user_config.h"
+#include "lra/lra_util.h"
+
+#include "devices/adxl355.h"
+
+/* external variables */
+
+extern LRA_DualBuf_t lra_acc_dbuf;
+
+#endif /* INC_LRA_SPI_DEVICES_H_ */
---
+++ Core/Src/lra/lra_spi_devices.c
@@ -0,0 +1,14 @@
+/*
+ * lra_spi_devices.c
+ *
+ *  Created on: Mar 22, 2023
+ *      Author: Dennis Liu
+ */
+
+/* includes */
+
+#include "lra/lra_spi_devices.h"
+
+/* extern variables definitions */
+
+LRA_DualBuf_t lra_acc_dbuf;
